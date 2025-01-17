77a9f3d4dab906c0d922bf789b6e1d3455139a51
ricckker <3356203629@qq.com>
2025-01-17 14:48:03 +0800

    2025/1/17

---
+++ code/py基础语法/数据类型.py
@@ -0,0 +1,22 @@
+def string_example():
+    pass
+
+
+def list_example():
+    pass
+
+
+def dict_example():
+    pass
+
+
+def tuple_example():
+    pass
+
+
+def set_example():
+    pass
+
+
+if __name__ == '__main__':
+    string_example()

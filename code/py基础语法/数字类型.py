7b1ec625b3e96f2247b37e49b873e9f49e307f32
ricckker <3356203629@qq.com>
2025-01-17 13:53:32 +0800

    2025/1/17

---
+++ code/py基础语法/数字类型.py
@@ -0,0 +1,11 @@
+def int_number_example():
+    number1 = 78
+    print("小整数的输出:", number1)
+    number2 = 66666666666666666666666666666
+    print("大整数的输出:", number2)
+
+    return
+
+
+if __name__ == '__main__':
+    int_number_example()

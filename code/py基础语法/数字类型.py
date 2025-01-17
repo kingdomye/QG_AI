9633ef1d3102966ce4539eff0cc6ae5e5be47fba
ricckker <3356203629@qq.com>
2025-01-17 13:58:53 +0800

    2025/1/17

--- code/py基础语法/数字类型.py
+++ code/py基础语法/数字类型.py
@@ -1,8 +1,15 @@
 def int_number_example():
+    # int类型数据范围
     number1 = 78
-    print("小整数的输出:", number1)
+    print("Small integer output:", number1)
     number2 = 66666666666666666666666666666
-    print("大整数的输出:", number2)
+    print("Big integer output:", number2)
+
+    # int类型四则运算
+    print("addition", number1 + number2)
+    print("subtraction", number1 - number2)
+    print("multiplication", number1 * number2)
+    print("division", number2 / number1)
 
     return
 

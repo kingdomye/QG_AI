b235c08499eb810d9b849d3bf0cf554bea0491a7
ricckker <3356203629@qq.com>
2025-01-18 11:56:45 +0800

    2025/1/18

--- code/py基础语法/推导式.py
+++ code/py基础语法/推导式.py
@@ -1,2 +1,4 @@
-list1 = [x for x in range(10)]
-print(list1)
+print("列表推导式", [x for x in range(10)])
+print("字典推导式", {x: x for x in (1, 2, 3)})
+print("集合推导式", {x for x in 'abracadabra' if x not in 'abc'})
+print("元组推导式", tuple((x for x in range(1, 10))))

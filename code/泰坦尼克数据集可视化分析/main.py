81e2a5274befbbdd68ff26eedee9c01185ce7189
ricckker <3356203629@qq.com>
2025-01-26 20:39:46 +0800

    2025/1/26

--- code/泰坦尼克数据集可视化分析/main.py
+++ code/泰坦尼克数据集可视化分析/main.py
@@ -17,7 +17,6 @@ class TitanicData:
         plt.pie(n, autopct='%.2f%%', labels=['death', 'survival'], pctdistance=0.4, labeldistance=0.6,
                 shadow=True, explode=[0, 0.1], textprops=dict(size=15))
         plt.title('all_survival_rate')
-        plt.Text(0.5, 1.0, 'all_survival_rate')
         plt.show()
 
     def sex_survival_rate(self):
@@ -34,7 +33,6 @@ class TitanicData:
         axes2.pie(sex_count.loc['male'], autopct='%.2f%%', labels=['death', 'survival'], pctdistance=0.4, labeldistance=0.6,
                   shadow=True, explode=[0, 0.1], textprops=dict(size=15), colors=['#2E8B57', '#AFEEEE'])
         axes2.set_title('male survival rate')
-        plt.Text(0.5, 1.0, 'male survival rate')
         plt.show()
 
     def age_survival_rate(self):
@@ -58,7 +56,48 @@ class TitanicData:
         plt.legend()
         plt.show()
 
+    def embarked_survival_rate(self):
+        embarked_count = self.df.groupby(by='Embarked')['Survived'].value_counts()
+        plt.figure(figsize=(3 * 5, 5))
+
+        axes1 = plt.subplot(1, 3, 1)
+        axes1.pie(embarked_count.loc['C'][::-1], autopct='%.2f%%', labels=['death', 'survival'], pctdistance=0.4, labeldistance=0.6,
+                  shadow=True, explode=[0, 0.1], textprops=dict(size=15), colors=['#9400D3', '#FFB6C1'], startangle=45)
+        axes1.set_title('Passenger survival rate in Cherbourg, France')
+
+        axes2 = plt.subplot(1, 3, 2)
+        axes2.pie(embarked_count.loc['Q'], autopct='%.2f%%', labels=['death', 'survival'], pctdistance=0.4, labeldistance=0.6,
+                  shadow=True, explode=[0, 0.1], textprops=dict(size=15), colors=['#4169E1', '#AFEEEE'])
+        axes2.set_title('Passenger survival rate in Queenstown, Ireland')
+
+        axes3 = plt.subplot(1, 3, 3)
+        axes3.pie(embarked_count.loc['S'], autopct='%.2f%%', labels=['death', 'survival'], pctdistance=0.4, labeldistance=0.6,
+                  shadow=True, explode=[0, 0.1], textprops=dict(size=15), colors=['#698B69', '#76EE00'])
+        axes3.set_title('Passenger survival rate in Southampton, UK')
+        plt.show()
+
+    def pclass_survival_rate(self):
+        pclass_count = self.df.groupby(by='Pclass')['Survived'].value_counts()
+        plt.figure(figsize=(3 * 5, 5))
+
+        axes1 = plt.subplot(1, 3, 1)
+        axes1.pie(pclass_count.loc[1][::-1], autopct='%.2f%%', labels=['death', 'survival'], pctdistance=0.4,
+                  labeldistance=0.6,
+                  shadow=True, explode=[0, 0.1], textprops=dict(size=15), colors=['#9400D3', '#FFB6C1'], startangle=45)
+        axes1.set_title('Survival rate of first-class passengers')
+
+        axes2 = plt.subplot(1, 3, 2)
+        axes2.pie(pclass_count.loc[2], autopct='%.2f%%', labels=['death', 'survival'], pctdistance=0.4, labeldistance=0.6,
+                  shadow=True, explode=[0, 0.1], textprops=dict(size=15), colors=['#4169E1', '#AFEEEE'])
+        axes2.set_title('Survival rate of second-class passengers')
+
+        axes3 = plt.subplot(1, 3, 3)
+        axes3.pie(pclass_count.loc[3], autopct='%.2f%%', labels=['death', 'survival'], pctdistance=0.4, labeldistance=0.6,
+                  shadow=True, explode=[0, 0.1], textprops=dict(size=15), colors=['#698B69', '#76EE00'])
+        axes3.set_title('Survival rate of third-class passengers')
+        plt.show()
+
 
 if __name__ == '__main__':
     titanic = TitanicData()
-    titanic.age_survival_rate()
+    titanic.pclass_survival_rate()

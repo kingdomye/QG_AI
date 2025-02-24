a2caede0f571660b98d125a42a9cef1daaccc91f
ricckker <3356203629@qq.com>
2025-02-24 09:31:12 +0800

    2025/2/24

--- code/07、K-means类封装/main.py
+++ code/07、K-means类封装/main.py
@@ -10,22 +10,34 @@ def calculate_distance(x, y):
 
 
 class KMeans:
-    def __init__(self):
+    def __init__(self, n_clusters=3):
         self.data = None
-        self.n_clusters = None
+        self.n_clusters = n_clusters
         self.max_iter = None
+        self.X = None
+        self.Y = None
 
     def read_data(self, filename="./iris_data/iris.csv"):
         self.data = pd.read_csv(filename)
+        self.X, self.Y = self.data.iloc[:, 1: -1], self.data.iloc[:, -1]
+        self.X = self.X.values
+
+    def get_clusters(self, clusters_center):
+        distances = np.linalg.norm(self.X[:, np.newaxis] - clusters_center, axis=2)
+        new_clusters = np.argmin(distances, axis=1)
+        return new_clusters
 
     def classify(self):
-        X, Y = self.data.iloc[:, 1:-1], self.data.iloc[:, -1]
-        clusters_center = X.sample(n=self.n_clusters).to_numpy()
-        print(clusters_center)
+        clusters_center = np.array(random.sample(self.X.tolist(), self.n_clusters))
+        new_clusters = self.get_clusters(clusters_center)
+        print(new_clusters)
+
+    def test(self):
+        print(self.X[:, np.newaxis])
 
 
 if __name__ == '__main__':
     kmeans = KMeans()
-    kmeans.n_clusters = 3
     kmeans.read_data()
+    # kmeans.test()
     kmeans.classify()
